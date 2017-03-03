9899987a33ee1d5fbf79aa525e97b4f5668aab08
huangzy <huangzhiyang166@gmail.com>
2017-03-03 10:09:16 +0800

    modify

--- gulpfile.js
+++ gulpfile.js
@@ -22,22 +22,39 @@ var getOutputDir = function(env){
 		production : "./build/production"
 	}[env];
 };
-var upload = function(env){
+var upload = function(env,type){
 	var js_filename = `/${env}/js/${Config.output.filename}.js`;
 	var css_filename = `/${env}/css/${Config.output.filename}.css`;
-	Util.upload({
-		file : BUILD_DIR + js_filename,
-		host : env,
-		remotePath : "/js"
-	})
-	Util.upload({
-		file : BUILD_DIR + css_filename,
-		host : env,
-		remotePath : "/css"
-	})
+	if(!type){
+		Util.upload({
+			file : BUILD_DIR + js_filename,
+			host : env,
+			remotePath : "/js"
+		})
+		Util.upload({
+			file : BUILD_DIR + css_filename,
+			host : env,
+			remotePath : "/css"
+		})
+	}else if(type=="js"){
+		Util.upload({
+			file : BUILD_DIR + js_filename,
+			host : env,
+			remotePath : "/js"
+		})
+	}else if(type=="css"){
+		Util.upload({
+			file : BUILD_DIR + css_filename,
+			host : env,
+			remotePath : "/css"
+		})
+	}
 }
 
 var Compile = function(env){
+	var envArr = env.split(":");
+	env = env[0];
+	var type = envArr[1];
 	var output = getOutputDir(env);
 	var webpackConfig = getWebpackConfig(env);
 	return gulp.src(Config.entry)
@@ -48,7 +65,7 @@ var Compile = function(env){
 		.pipe(gulp.dest(output))
 		.on('end', function(){
 			console.log("compile finish");
-			//if(env!=="develop") upload(env);
+			if(env!=="develop") upload(env,type);
 		});
 };
 
@@ -57,11 +74,41 @@ gulp.task("test",function(){
 	del("./build/test");
 	Compile("test");
 })
+gulp.task("test:css",function(){
+	del("./build/test/css");
+	Compile("test:css");
+})
+gulp.task("test:js",function(){
+	del("./build/test/js");
+	Compile("test:js");
+})
+
+
 gulp.task("release",function(){
 	del("./build/release");
 	Compile("release");
 })
+gulp.task("release:css",function(){
+	del("./build/release/css");
+	Compile("release:css");
+})
+
+gulp.task("release:js",function(){
+	del("./build/release/js");
+	Compile("release:js");
+})
+
+
+
 gulp.task("production",function(){
 	del("./build/production");
 	Compile("production");
+})
+gulp.task("production:css",function(){
+	del("./build/production/css");
+	Compile("production:css");
+})
+gulp.task("production",function(){
+	del("./build/production/js");
+	Compile("production:js");
 })
