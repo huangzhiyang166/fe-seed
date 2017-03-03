66146a89694d9b3ea52db22453c9abd0f84a97be
huangzy <huangzhiyang166@gmail.com>
2017-03-03 11:29:15 +0800

    m

--- webpack.config.js
+++ webpack.config.js
@@ -45,6 +45,12 @@ module.exports = function(env){
 				test : /\.css$/,
 				loader : ExtractTextPlugin.extract("style","css?sourceMap!cssnext!postcss")
 			},{
+				test : /\.less$/,
+				loader : ExtractTextPlugin.extract("style","css?sourceMap!cssnext!postcss!less")
+			},{
+				test : /\.sass|scss$/,
+				loader : ExtractTextPlugin.extract("style","css?sourceMap!cssnext!postcss!sass")
+			},{
 				test : /\.(ttf|eot|svg|woff(2)?)(\?[a-z0-9=&.]+)?$/,loader : 'file-loader'
 			},{
 				test : /\.(png|jpe?g|gif)$/,loader : 'url-loader?limit=8192&name=images/[name]-[hash].[ext]'
